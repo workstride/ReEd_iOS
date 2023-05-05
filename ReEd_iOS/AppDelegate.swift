d9b51fe12cc6fb799ae1545ce9ead82983e12a9c
Garr15 <maxgun4757@naver.com>
2023-05-06 02:58:01 +0900

    Initial Commit

---
+++ ReEd_iOS/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ReEd_iOS
+//
+//  Created by 김건우 on 2023/05/06.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+    
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.makeKeyAndVisible()
+            window?.backgroundColor = .systemBackground
+            window?.rootViewController = ViewController()
+            
+            return true
+        }
+}
+
---
+++ ReEd_iOS/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  ReEd_iOS
+//
+//  Created by 김건우 on 2023/05/06.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class ViewController: UIViewController {
+    
+    private let logoImage = UIImageView().then {
+        $0.image = UIImage(named: "logo")
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let emailTextField = UITextField().then {
+        $0.placeholder = "Email"
+        $0.keyboardType = .emailAddress
+        $0.autocapitalizationType = .none
+        $0.borderStyle = .roundedRect
+        $0.returnKeyType = .next
+    }
+    
+    private let passwordTextField = UITextField().then {
+        $0.placeholder = "Password"
+        $0.isSecureTextEntry = true
+        $0.borderStyle = .roundedRect
+        $0.returnKeyType = .done
+    }
+    
+    private let loginButton = UIButton().then {
+        $0.setTitle("Login", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .blue
+        $0.layer.cornerRadius = 10
+        $0.layer.masksToBounds = true
+    }
+    
+    private let forgotPwbutton = UIButton().then {
+        $0.setTitle("비밀번호를 잊으셨나요?", for: .normal)
+        $0.setTitleColor(.blue, for: .normal)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupSubviews()
+        setupConstraints()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+             view.addGestureRecognizer(tapGesture)
+         }
+         // 키보드를 내리는 메서드
+         @objc private func dismissKeyboard() {
+             view.endEditing(true)
+         }
+
+    
+    
+    private func setupSubviews() {
+        view.addSubview(logoImage)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(forgotPwbutton)
+    }
+    
+    private func setupConstraints() {
+        
+        logoImage.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(100)
+            make.width.equalTo(200)
+        }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(150)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(50)
+        }
+        
+        forgotPwbutton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(loginButton.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(40)
+        }
+        
+    }
+}
